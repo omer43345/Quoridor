0395eff4fe6a051e2326d48b629cd3edfcc2c63a
omer hadad <omer.hadad1@gmail.com>
2023-01-16 19:37:09 +0200

    create main page scene

--- Quoridor/src/main/java/com/example/quoridor/HelloController.java
+++ Quoridor/src/main/java/com/example/quoridor/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.quoridor;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -10,5 +11,8 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavFX Application!");
+    }
+
+    public void startGame(ActionEvent actionEvent) {
     }
 }
--- Quoridor/src/main/java/com/example/quoridor/Main.java
+++ Quoridor/src/main/java/com/example/quoridor/Main.java
@@ -3,18 +3,30 @@ package com.example.quoridor;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hell");
+        // use FXMLLoader to load the FXML file
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("main-page.fxml"));
+        // load the root node from the FXML file
+        Pane root = loader.load();
+        // create a scene with the root node
+        Scene scene = new Scene(root);
+        // set the scene to the stage
         stage.setScene(scene);
+        // set the title of the stage
+        stage.setTitle("Quoridor");
+
+        // show the stage
         stage.show();
+
     }
 
     public static void main(String[] args) {
